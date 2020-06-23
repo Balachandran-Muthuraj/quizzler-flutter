e27fb3548ab982fe014bf9a84d52f332b75662cc
Bala <rmbalachandar@gmail.com>
2020-06-23 14:31:35 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,6 +27,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> ScoreKeeper = [
+  ];
+  List<String> Questions=[
+    'Is Lee Sian Long is the Singapore PM',
+    'Is Narendra Modi is the India PM',
+    'Is Edappadi Palanisamy is the CM of Andra',
+  ];
+  int Questionnumber=0;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: EdgeInsets.all(15.0),
               child: Center(
                   child: Text(
-                'This is where the question text will go.',
+                Questions[Questionnumber],
                 style: TextStyle(fontSize: 20, color: Colors.white),
               )),
             ),
@@ -47,31 +56,54 @@ class _QuizPageState extends State<QuizPage> {
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(15.0),
-              child: Center(
-                child: FlatButton(
-                  color: Colors.green,
-                  child: Text('Yes',style: TextStyle(fontSize: 20,color:Colors.white),),onPressed: (){
-                    print('Press Yes');
-                },
+              child: FlatButton(
+                color: Colors.green,
+                child: Text(
+                  'Yes',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
+                onPressed: () {
+                 setState(() {
+                   Questionnumber++;
+                   print(Questionnumber);
+                   ScoreKeeper.add(  Icon(
+                     Icons.check,color: Colors.green,
+                   ));
+                 });
+                },
               ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(15.0),
-              child: Center(
-                child: FlatButton(
-                  color: Colors.red,
-                  child: Text('False',style: TextStyle(  fontSize: 20.0,
-                    color: Colors.white,),),onPressed: (){
-                  print('Press False');
-                },
+              child: FlatButton(
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
                 ),
+                onPressed: () {
+                 setState(() {
+                   
+                   Questionnumber++;
+                   print(Questionnumber);
+                   ScoreKeeper.add(  Icon(
+                     Icons.close,color: Colors.red,
+                   ));
+
+                 });
+                },
               ),
             ),
-          )
+          ),
+          Row(
 
+            children: ScoreKeeper,
+          )
         ],
       ),
     );
